17bf638df3e7ed5362129d40bfdac71f76b5d304
Hector Vizzuality <hectorvizzuality@MacBook-Air-de-Hector.local>
2017-07-10 15:50:28 +0200

    include information text

--- source/javascripts/app/stickySearchNav.js
+++ source/javascripts/app/stickySearchNav.js
@@ -8,12 +8,14 @@
       $('.tocify-wrapper').addClass('-fixed');
       $('.lang-selector').addClass('-fixed');
       $('#nav-button').addClass('-fixed');
+      $('.lang-selector-contain__text').addClass('-hidden');
     }
 
     if (window.scrollY < heightLimit) {
       $('.tocify-wrapper').removeClass('-fixed');
       $('.lang-selector').removeClass('-fixed');
       $('#nav-button').removeClass('-fixed');
+      $('.lang-selector-contain__text').removeClass('-hidden');
     }
   };
 
